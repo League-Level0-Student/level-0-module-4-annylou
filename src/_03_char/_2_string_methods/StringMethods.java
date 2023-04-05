ba4d3c896de8cc325d78e374174525ecd7a61f5c
league <league@imac-24.attlocal.net>
2023-04-05 16:55:04 -0700

    pi

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,43 +5,46 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-String character = "one";
+String word = "one";
 
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-char three = character.charAt(2);
+char three = word.charAt(2);
 	System.out.println(three);
 	
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-int length = character.length();
+int length = word.length();
 		System.out.println(length);
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
 		
-	int letter = 0;
-for(length=0; length<=2; length+=1) {
-three = character.charAt(0 + letter);
-System.out.println(three);
-letter++;
-	}
+		int number = 0;
+		for(length=0; length<=2; length+=1) {
+		three = word.charAt(0 + number);
+		System.out.println(three);
+		number++;
+			}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-
-	System.out.println(" " +three);
-	if(three>=2) {
-		System.out.println(" e is at index 2");
-	}
-	else if(three>=1) {
-		System.out.println(" n is at index 1");
-	}
-	else if(three>=0) {
-		System.out.println(" o is at index 0");
-	}
+		
+		
+		System.out.println(three);
 	
+		if(word.equals("one")) {
+			System.out.println("e is 2");
+		}
+		else if(word.equals("one")) {
+			System.out.println("n is 1");
+		}
+		else if(word.equals("one")) {
+			System.out.println("o is 0");
+		}
+	
+	 
 }
 }
 	
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -12,14 +12,23 @@ public class PiAloud {
 
 
     // 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
     // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+String pi = "3.141592653589793238";
     // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
     // Check the console shows 3.1
-
+char one = pi.charAt(0); char two = pi.charAt(1); char three = pi.charAt(2);
+System.out.println(one +"" +two +"" +three);
     // 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+int length = pi.length(); int twenty = 0;
 
+for(length=0; length<=19; length+=1) {
+char digits = pi.charAt(0 + twenty);
+System.out.println(digits);
+twenty++;
+	}
     // 5. Skip this step if your computer cannot play sounds.
     //if (canPlaySounds) {
         // Use the Sound.speak() method to speak each digit of Pi.
@@ -32,7 +41,7 @@ public class PiAloud {
     // *7. Compare the users' char to the next digit of Pi
     // *8. If they are correct, print out "correct". If they are not, print "incorrect"
 
-
+}
     /********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
     private static void pause(int seconds) {
