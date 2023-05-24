f97fc15cf0c94a68994454ed77708d1226832eb2
league <league@imac-24.attlocal.net>
2023-05-24 16:56:12 -0700

    pong

--- src/_99_extra/pong/Pong.java
+++ src/_99_extra/pong/Pong.java
@@ -1,14 +1,18 @@
 package _99_extra.pong;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Pong extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
+    static final int WIDTH = 600;
+    static final int HEIGHT = 470;
+    PImage backgroundImage;
 int x;
 int y;
 int xSpeed;
 int ySpeed;
+int paddleX = 260;
+int paddleY = 441;
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -16,23 +20,31 @@ int ySpeed;
 
     @Override
     public void setup() {
-    	
+    	backgroundImage = loadImage("grey.png");
     }
 
     @Override
     public void draw() {
-    fill(0,0,255);
-    noStroke();
-    ellipse(100, 100, 10,10);
-    if(x>width) {
-    	xSpeed = -xSpeed;
+    	image(backgroundImage, 0,0, 600,470);
+        fill(53,122,185);
+        noStroke();
+        ellipse(9, 9, 17,17);
+        if(x>width) {
+        	xSpeed = -xSpeed;
+        }
+        if(y>height) {
+        	ySpeed = -ySpeed;
+        }
+        paddleX=mouseX;
+        fill(136,185,78);
+        rect(paddleX,paddleY, 80,29);
+         
     }
-    if(y>height) {
-    	ySpeed = -ySpeed;
-    }
-    
-    }
-
+boolean intersects(int x, int y, int paddleX, int paddleY, int paddleLength) {
+	if(y>paddleY && x>paddleX && x<paddleX+paddleLength) {
+		return true;
+	}
+}
     static public void main(String[] args) {
         PApplet.main(Pong.class.getName());
     }
